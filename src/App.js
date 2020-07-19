99a76dccf1be6249a2a21c5afaff78bdfc6aef2f
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-07-19 21:25:05 +0200

    DEV-03: http service and some dependencies

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import Users from './components/Users';
-
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <Switch>
-      <Route path="/users" component={Users} />
-    </Switch>
+    <React.Fragment>
+      <ToastContainer />
+      <Switch>
+        <Route path="/users" component={Users} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
