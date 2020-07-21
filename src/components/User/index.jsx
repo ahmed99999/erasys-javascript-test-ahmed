a7efe920f116f037f3f0d87226cd158cf3b85df3
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-07-21 19:28:44 +0200

    DEV-06: adding the detailed information and onClick event

--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-const User = ({ user, detailed }) => {
+const User = ({ user }) => {
+    const [detailed, setDetailed] = useState(false);
+
+    const ButtonText = !detailed ? "Show more information" : "Show less information";
+    const lastLogin = (new Date() - new Date(user.last_login)) / (1000 * 60);
     return (
         <React.Fragment>
-            {!detailed && <div className="card" style="width: 18rem;">
+            <div className="card" style="width: 18rem;">
                 <img src={user.picture.url} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title"> {user.personal.age} | {user.name}</h5>
-                    <p className="card-text">{user.headline}</p>
-                    <button className="btn btn-primary">Show more information</button>
+                    <h5 className="card-title">{detailed && `${user.personal.age} | `}{user.name}</h5>
+                    <p className="card-text">{detailed && user.headline}</p>
                 </div>
-            </div>}
+                {detailed && <ul className="list-group list-group-flush">
+                    <li className="list-group-item">{user.location.city}</li>
+                    <li className="list-group-item">{user.location.distance} km</li>
+                    <li className="list-group-item">active {lastLogin} minutes ago</li>
+                </ul>}
+                <div className="card-body">
+                    <button className="btn btn-primary" onClick={() => setDetailed(!detailed)}>{ButtonText}</button>
+                </div>
+            </div>
         </React.Fragment>
     );
 }
