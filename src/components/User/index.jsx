100fd5b0587000a41bb68632af17523b7f39b82f
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-07-25 15:59:12 +0200

    DEV-06: refactoring the last login logic

--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
+import moment from 'moment';
 import './style.css';
 
 const User = ({ user }) => {
     const [detailed, setDetailed] = useState(false);
 
     const ButtonText = !detailed ? "Show more information" : "Show less information";
-    const lastLoginMinutes = (new Date() - new Date(user.last_login)) / (1000 * 60);
 
-    const lastLoginUnitesCount = (lastLoginMinutes / (60 * 24 * 30) > 1) ? `${Math.round(lastLoginMinutes / (60 * 24 * 30))} months` :
-        (lastLoginMinutes / (60 * 24) > 1) ? `${Math.round(lastLoginMinutes / (60 * 24))} days` :
-            ((lastLoginMinutes / 60) > 1) ? `${Math.round(lastLoginMinutes / 60)} hours` : `${Math.round(lastLoginMinutes)} minutes`;
+    const lastLoginUnitesCount = () => {
+        const lastLogin = new Date(user.last_login);
+        const differenceInMonths = moment().diff(lastLogin, 'months');
+        const differenceInDays = moment().diff(lastLogin, 'days');
+        const differenceInHours = moment().diff(lastLogin, 'hours');
+        const differenceInMinutes = moment().diff(lastLogin, 'minutes');
+
+        if (differenceInMonths > 1) return `${differenceInMonths} months`;
+        if (differenceInDays > 1) return `${differenceInDays} days`;
+        if (differenceInHours > 1) return `${differenceInHours} hours`;
+        if (differenceInMinutes > 1) return `${differenceInMinutes} minutes`;
+    };
 
     const { url: imageUrl } = { ...user.picture };
     const onlineStatusClass = (user.online_status === 'ONLINE') ? 'logged-in' : 'logged-out';
     return (
-        <div className="card" style={{ width: "18rem", margin: '13px' }}>
+        <div className="card card-container-class">
             <img src={imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{`${user.personal.age} | ${user.name}`}</h5>
@@ -23,7 +32,7 @@ const User = ({ user }) => {
             </div>
             {detailed && <ul className="list-group list-group-flush">
                 <li className="list-group-item">{user.location.city}</li>
-                <li className="list-group-item">{`active ${lastLoginUnitesCount} ago`}</li>
+                <li className="list-group-item">{`active ${lastLoginUnitesCount()} ago`}</li>
             </ul>}
             <div className="card-body">
                 <button className="btn btn-primary" onClick={() => setDetailed(!detailed)}>{ButtonText}</button>
